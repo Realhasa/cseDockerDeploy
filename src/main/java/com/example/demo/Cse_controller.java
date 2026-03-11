2dbb798f90e180189de885b276c63ca95a43d4f4
rialhasan <realhasan74@gmail.com>
2026-03-11 20:15:44 +0530

    second cmmit

--- src/main/java/com/example/demo/Cse_controller.java
+++ src/main/java/com/example/demo/Cse_controller.java
@@ -1,5 +1,13 @@
 package com.example.demo;
 
-public class Cse_controller {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class Cse_controller {
+	
+	@GetMapping("/add")
+	public int cse_add() {
+		return "WELCOME TO CSE STAFF HOME PAGE";
+	}
 }
